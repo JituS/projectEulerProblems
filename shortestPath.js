5c114a0e43f70610003a5d6d53d6bde87d0408f5
jitendra sachdeva <jitendrs@thoughtworks.com>
2016-12-03 01:07:04 +0530

    improving code quality

--- shortestPath.js
+++ shortestPath.js
@@ -1,33 +1,18 @@
 var fs = require('fs');
-
 function dataShaper(data){
 	return data.map(function(each){
 		return each.split(' ').map(function(e){return +e;});
-	});
+	}).reverse();
 }
-
 var data = dataShaper(fs.readFileSync('data.txt', 'utf8').split('\n'));
-
-function max(array, number){
-	return Math.max.apply(this, array);
-}
 
 function longestPath(data) {
 	return data.reduce(function(pv, cv){
-		var result = [];
-		pv.forEach(function(each, index){
-			var left = each + cv[index];
-			var right = each + cv[index+1];
-			if(index){
-				if(result[result.length-1] < left){
-					result[result.length-1] = left;
-				}
-			}else{
-				result.push(left);
-			}
-			result.push(right);
+		return cv.map(function(each, index){
+			var left = each + pv[index];
+			var right = each + pv[index + 1];
+			return (left > right) ? left : right;
 		});
-		return result;
 	});
 }
-console.log(max(longestPath(data)))
+console.log(longestPath(data));
